87280524e621d2ecdb38c116053eed07a6fdee1b
JPB <jbanas9124@gmail.com>
2021-04-22 23:49:30 -0500

    html showing and connected to stylesheets, heroku deployed

--- routes/html/htmlRoutes.js
+++ routes/html/htmlRoutes.js
@@ -4,16 +4,16 @@ const path = require('path');
 
 //create html routes
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 //create notes routes
 router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
+  res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
 //create html routes
 router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const express = require('express');
 //tell heroku which port to use, add port to listen method below
 const PORT = process.env.PORT || 3001;
 
-//all our route functionality for later
-const routes = require('/routes/route.js');
-
 //instantiate express
 const app = express();
 
 //modularization started for organization
-const apiRoutes = require('/routes/apiRoutes');
-const htmlRoutes = require('/routes/htmlRoutes');
+// const apiRoutes = require('./routes/api/apiRoutes');
+const htmlRoutes = require('./routes/html/htmlRoutes');
+
+//access to the stylesheets and js files in 'public' folder
+app.use(express.static('public'));
 
 //parse incoming data through express into json
 app.use(express.urlencoded({ extended: true }));
@@ -20,7 +20,7 @@ app.use(express.json());
 
 //if the api is '/api' it will look in apiRoutes.js
 //if '/' then htmlRoutes
-app.use('/api', apiRoutes);
+// app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 
