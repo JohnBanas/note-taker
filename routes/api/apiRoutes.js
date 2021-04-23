268b24907da5e721aabb3f7e7e839dba483a4618
JPB <jbanas9124@gmail.com>
2021-04-22 23:58:34 -0500

    initial setup for apiRoutes

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -1,2 +1,12 @@
+//dependencies
+const fs = require("fs");
+const path = require("path");
+
+//the data to manipulate
+const db = require('../../db/db.json');
+
+//give unique id to objects
+var getUid = require('get-uid');
+
 
 
