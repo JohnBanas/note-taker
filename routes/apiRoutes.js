28d56296e42857a43972c8472581d687849e9b6c
JPB <jbanas9124@gmail.com>
2021-04-22 22:50:41 -0500

    initial setup for server.js to control different routes and apis

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,21 +10,13 @@ const routes = require('/routes/route.js');
 //instantiate express
 const app = express();
 
-//modularization started for organization
-const apiRoutes = require('/routes/apiRoutes');
-const htmlRoutes = require('/routes/htmlRoutes');
-
 //parse incoming data through express into json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-//if the api is '/api' it will look in apiRoutes.js
-//if '/' then htmlRoutes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
 
 
 //listener for apis
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
